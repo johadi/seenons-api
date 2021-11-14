63e8b0974fdb2a318e3c7a40cef1307c448f14c7
johadi <jim.hadi10@gmail.com>
2021-11-14 22:46:03 +0100

    Fix failing e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import * as mongoose from 'mongoose';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let mongod: MongoMemoryServer;
+  let moduleFixture: TestingModule;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+    // This will create an new instance of "MongoMemoryServer" and automatically start it
+    mongod = await MongoMemoryServer.create();
+
+    const uri = mongod.getUri();
+
+    moduleFixture = await Test.createTestingModule({
+      imports: [MongooseModule.forRoot(uri), AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterEach(async () => {
+    await moduleFixture.close();
+    await mongoose.disconnect();
+    await mongod.stop();
   });
 
   it('/ (GET)', () => {
